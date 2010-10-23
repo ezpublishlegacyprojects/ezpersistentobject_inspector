98133a076ebaa6e3ff2972a395e3c8a293011ddc
Gaetano Giunta <gg@ez.no>
2010-10-23 12:45:10 +0000

    - import current code from ezdebug
    git-svn-id: http://svn.projects.ez.no/ezpersistentobject_inspector@1 bcf93d68-24d8-4843-a820-cac278487666

---
+++ trunk/extension/ezinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+class ezpersistentobject_inspectorInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "eZPersistentObject Inspector",
+            'Version' => "0.1-beta",
+            'Copyright' => "Copyright (C) 2010 G. Giunta",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+
+?>
