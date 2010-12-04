6d4215566625cad41cbd578603e49f338d01d5bd
Gaetano Giunta <gg@ez.no>
2010-12-04 18:13:56 +0000

    git-svn-id: http://svn.projects.ez.no/ezpersistentobject_inspector@28 bcf93d68-24d8-4843-a820-cac278487666

--- trunk/extension/ezpersistentobject_inspector/ezinfo.php
+++ trunk/extension/ezpersistentobject_inspector/ezinfo.php
@@ -6,7 +6,7 @@ class ezpersistentobject_inspectorInfo
     {
         return array(
             'Name' => "<a href=\"http://projects.ez.no/ezpersistentobject_inspector\">eZPersistentObject Inspector</a>",
-            'Version' => "0.3-beta",
+            'Version' => "0.3-dev",
             'Copyright' => "Copyright (C) 2010 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
